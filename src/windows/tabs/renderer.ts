7d4ae03537dbe3041179ac7407ccaf7793c2e9c3
lijialong <lijialong@kingsoft.com>
2020-04-28 16:42:04 +0800

    tabs

--- src/windows/tabs/renderer.ts
+++ src/windows/tabs/renderer.ts
@@ -1,12 +1,58 @@
-import TabGroup from 'electron-tabs'
 import './index.css'
+import ElectronTabs from 'electron-tabs'
 
-const tabGroup = new TabGroup()
-
-const tab = tabGroup.addTab({
-  title: 'baidu',
-  src: 'https://www.baidu.com',
-  visible: true
+const tabGroup = new ElectronTabs({
+  // newTab: {
+  //   title: 'New Tab'
+  // }
 })
 
-console.log(tab)
+// tabGroup.addTab({
+//   title: 'Google',
+//   src: 'http://google.com'
+// })
+
+// tabGroup.addTab({
+//   title: 'Baidu',
+//   src: 'https://baidu.com'
+// })
+
+function createInterceptedTab (url: string): ElectronTabs.Tab {
+  const tab = tabGroup.addTab({
+    title: 'edu-back',
+    src: url,
+    visible: true,
+    active: true
+  })
+
+  tab.on('webview-dom-ready', tab => {
+    const { webview } = tab
+    let url = ''
+    // webview.addEventListener('will-navigate', (event: any) => {
+    //   console.log('will-navigate', event)
+    // })
+    // webview.addEventListener('will-navigate-in-page', (event: any) => {
+    //   console.log('will-navigate-in-page', event)
+    // })
+    // webview.addEventListener('did-navigate', (event: any) => {
+    //   console.log('did-navigate', event)
+    // })
+    webview.addEventListener('did-navigate-in-page', (event: any) => {
+      if (url) {
+        url = ''
+      } else {
+        url = event.url
+        setTimeout(() => {
+          createInterceptedTab(event.url)
+        })
+      }
+      webview.goBack()
+      console.log('did-navigate-in-page', event)
+      // setTimeout(() => webview.goBack(), 1000)
+    })
+  })
+
+  return tab
+}
+
+createInterceptedTab('http://edu-back.wps.cn:3000')
